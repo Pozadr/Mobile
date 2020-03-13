2fc17313da28837e786148a88c1947cc2e332891
Pozadr <adpozor@gmail.com>
2020-03-13 19:59:02 +0100

    Additional exercises

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         // Creating a new object using class MobilePhone.
-        MobilePhone phone = new MobilePhone("200g", 700);
+        /*MobilePhone phone = new MobilePhone("200g", 700);
+        MobilePhone oldPhone = new MobilePhone("4000g", 100);
+        MobilePhone mediumPhone = new MobilePhone("600g", 340);
 
         phone.displayValues();
 
@@ -18,7 +20,17 @@ public class Main {
         int newPrice = phone.newPrice(5);
         System.out.print("\nPrice from method newPrice: " + newPrice);
 
+        phone.checkPrice();
+        oldPhone.checkPrice();
+        mediumPhone.checkPrice();
+        */
 
+        // Other tasks/exercises
+        MobilePhone otherTasks = new MobilePhone("100g", 1000);
+        otherTasks.displayEvenNumbers(17, 33);
+        otherTasks.isDivided(1,11);
+        System.out.println("Sum: " + otherTasks.getEvenSum(5, 9));
+        otherTasks.getNumber(15, -3);
     }
 }
 
@@ -56,4 +68,58 @@ class MobilePhone {
         return this.price * number;
     }
 
+    public void checkPrice() {
+        if (this.price < 200) {
+            System.out.println("Price is very good.");
+        } else if (this.price <300){
+            System.out.println("Price is good.");
+        } else if (this.price <550) {
+            System.out.println("Price could be lower.");
+        } else {
+                System.out.println("This phone is too expensive.");
+        }
+    }
+    public void displayEvenNumbers(int min, int max){
+        for(int i=min; i<=max; i++){
+            if(i%2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public void isDivided(int min, int max) {
+        System.out.println("\nNumbers form range " + min + ":" + max + " divided by 3: ");
+        for(int i=min; i <= max; i++) {
+            if (i%3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\nNumbers form range " + min + ":" + max + " divided by 5: ");
+        for(int i=min; i <= max; i++) {
+            if (i%5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public int getEvenSum(int min, int max){
+        System.out.println("\nSum of numbers in range " + min + ":" + max + " divided by 2: ");
+        int x = 0;
+        for(int i=min; i <= max; i++) {
+            if (i%2 == 0) {
+                x += i;
+            }
+        }
+        return x;
+    }
+    public void getNumber(int min, int max){
+        int  helpFlag = 0;
+        if (max < min){
+            helpFlag = min;
+            min = max;
+            max = helpFlag;
+        }
+        for(int i=max; i>=min; i--){
+            System.out.print(i + " ");
+        }
+    }
 }
